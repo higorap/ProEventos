0651e332fb01ca58b37a635924afcff2a8f46760
higor <h-igor-ap2012@hotmail.com>
2022-01-30 10:05:28 -0300

    V2Sql

--- Back/src/ProEventos.api/Controllers/EventoController.cs
+++ Back/src/ProEventos.api/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.api.Data;
 using ProEventos.api.Models;
 
 namespace ProEventos.api.Controllers
@@ -12,45 +13,29 @@ namespace ProEventos.api.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-       public IEnumerable<Evento> _evento = new Evento []{
-                new Evento()
-              {
-                EventoId= 1,
-                Tema = "angular",
-                Local="SP",               
-                Lote="primeiro lote",
-                QtdPessoas=200,
-                DataEvento= DateTime.Now.AddDays(2).ToString()
-              },
-                new Evento()
-              {
-                EventoId= 2,
-                Tema = "CSS",
-                Local="BH",               
-                Lote="primeiro lote",
-                QtdPessoas=2020,
-                DataEvento= DateTime.Now.AddDays(32).ToString()
-              }
-            };
+    
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 
-        [HttpGet] 
+        [HttpGet]
         public IEnumerable<Evento> Get()
-        {     
-            return _evento;
+        {
+            return _context.Eventos;
         }
-         [HttpGet("{id}")] 
-        public IEnumerable<Evento> GetById(int id)
-        {     
-            return _evento.Where(Evento=>Evento.EventoId==id);
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(
+              Evento => Evento.EventoId == id);
         }
 
-         [HttpPost] 
+        [HttpPost]
         public string Post()
-        {     
+        {
             return "value-Post";
         }
     }
